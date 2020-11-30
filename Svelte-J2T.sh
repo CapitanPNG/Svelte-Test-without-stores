13ea28295f79acf5bcc3aa5a1ebfa69830fda2b4
CapitanPNG <pierniccologiannelli@gmail.com>
2020-11-30 10:55:54 +0100

    v0

--- Svelte-J2T.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm install --save-dev @tsconfig/svelte typescript svelte-preprocess svelte-check
-
-npm install @rollup/plugin-typescript --save-dev
-
-node scripts/setupTypeScript.js
-
-npm install
